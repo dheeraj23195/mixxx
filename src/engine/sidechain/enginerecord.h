70b43687a739d11b2a102ec1a5f99dcf73b68bae
Dheeraj Arora <dheerajarora@Dheerajs-MacBook-Air.local>
2026-02-01 02:58:41 +0530

    enabled mono support for encoder across all encoders that threw an error on building after changing base initEncoder() fucntion to accept number of channels

--- src/engine/sidechain/enginerecord.h
+++ src/engine/sidechain/enginerecord.h
@@ -6,6 +6,7 @@
 #include "audio/types.h"
 #include "control/pollingcontrolproxy.h"
 #include "encoder/encoder.h"
+#include "encoder/encodersettings.h"
 #include "encoder/encodercallback.h"
 #include "engine/sidechain/sidechainworker.h"
 #include "preferences/usersettings.h"
@@ -86,4 +87,5 @@ class EngineRecord : public QObject, public EncoderCallback, public SideChainWor
     quint64 m_cueTrack;
     bool m_bCueIsEnabled;
     bool m_bCueUsesFileAnnotation;
+    bool m_bRecordMono=false;
 };
